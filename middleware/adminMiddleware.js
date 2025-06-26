689bcb6682c834b7a44b3d2633421b302f05b7c4
damianczarnecki93 <damian.czarnecki@e-dekor.pl>
2025-06-26 09:28:58 +0200

    Update adminMiddleware.js

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,22 +1,14 @@
-// middleware/adminMiddleware.js
 const User = require('../models/User');
 
-// To middleware zakłada, że `authMiddleware` został już użyty wcześniej.
-// Sprawdza, czy zalogowany użytkownik ma rolę 'admin'.
-const adminMiddleware = async (req, res, next) => {
+module.exports = async function(req, res, next) {
     try {
-        // Zakładamy, że authMiddleware dodał już req.user
         const user = await User.findById(req.user.id);
-
         if (user && user.role === 'admin') {
-            next(); // Użytkownik jest adminem, przejdź dalej
+            next();
         } else {
-            // Jeśli użytkownik nie jest adminem, zwróć błąd
-            return res.status(403).json({ msg: 'Brak uprawnień administratora.' });
+            return res.status(403).json({ msg: 'Brak uprawnień administratora' });
         }
     } catch (err) {
-        res.status(500).send('Błąd serwera przy weryfikacji uprawnień.');
+        res.status(500).send('Błąd serwera');
     }
 };
-
-module.exports = adminMiddleware;
