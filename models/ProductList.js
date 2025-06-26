433360e9fba40cc2d0d22050949b2f604537a0fd
damianczarnecki93 <damian.czarnecki@e-dekor.pl>
2025-06-26 09:28:10 +0200

    Update ProductList.js

--- models/ProductList.js
+++ models/ProductList.js
@@ -1,17 +1,19 @@
-// models/ProductList.js
 const mongoose = require('mongoose');
 
 const ProductListSchema = new mongoose.Schema({
-    // ZMIANA: Usunięto 'unique: true', aby użytkownik mógł mieć wiele list
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    listName: { type: String, required: true }, // Zmieniono z clientName na listName
-    items: [{
-        ean: String,
-        name: String,
-        description: String,
-        quantity: Number,
-        price: Number
-    }]
+    listName: { type: String, required: true },
+    clientName: { type: String, default: '' },
+    items: [
+        {
+            ean: String,
+            kod_kreskowy: String,
+            kod_produktu: String,
+            nazwa_produktu: String,
+            cena: String,
+            quantity: Number
+        }
+    ]
 }, { timestamps: true });
 
 module.exports = mongoose.model('ProductList', ProductListSchema);
