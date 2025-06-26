af29327718f7f8ac99d255800c88468979937188
damianczarnecki93 <damian.czarnecki@e-dekor.pl>
2025-06-26 11:06:01 +0200

    Update Inventory.js

--- models/Inventory.js
+++ models/Inventory.js
@@ -1,11 +1,21 @@
-// models/Inventory.js
 const mongoose = require('mongoose');
 
 const InventorySchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    inventoryName: {
+        type: String,
+        required: true,
+        default: () => `Inwentaryzacja z ${new Date().toLocaleDateString()}`
+    },
     items: [{
         ean: String,
-        name: String,
+        kod_kreskowy: String,
+        kod_produktu: String,
+        nazwa_produktu: String,
         quantity: Number
     }]
 }, { timestamps: true });
